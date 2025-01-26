c09f59c1d2938e477ca4cdc264f8370d16e405d1
Heru/ヘル <60049566+Heruzinyo@users.noreply.github.com>
2025-01-26 03:27:43 -0300

    Update avatar URLs in data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,5 +1,5 @@
 {
   "User_320975452199976961_PFP": "https://cdn.discordapp.com/avatars/320975452199976961/5fdfe0cdd8725f642fe1f715b9e67739.webp?size=1024",
-  "User_166319802263142401_PFP": "https://cdn.discordapp.com/avatars/166319802263142401/259b680dbb0a90528122a4692c8bc34c.webp?size=1024",
+  "User_166319802263142401_PFP": "https://cdn.discordapp.com/avatars/166319802263142401/82e46cae0372fda412a6af022c532eff.webp?size=1024",
   "User_1257744005270343680_PFP": "https://cdn.discordapp.com/avatars/1257744005270343680/fe88502f31ffcab38a4ba636064659a5.webp?size=1024"
 }
