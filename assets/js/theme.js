578d9d2e20915338f92a2c18c72322315fa2a027
Heru/ヘル <60049566+Heruzinyo@users.noreply.github.com>
2025-03-15 20:32:00 -0300

    Test Fix

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,12 +5,31 @@ const initThemeToggle = () => {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
 
+    // Determine base path
+    const getBasePath = () => {
+      // Get the current page path
+      const currentPath = window.location.pathname;
+      
+      // If we're in a subdirectory (has additional slashes after the domain)
+      if (currentPath.split('/').length > 2) {
+        // Count the number of directory levels from root
+        const levels = currentPath.split('/').filter(Boolean).length;
+        // Build the relative path prefix based on directory depth
+        return '../'.repeat(levels - 1);
+      } else {
+        // We're at the root level
+        return './';
+      }
+    };
+
+    const basePath = getBasePath();
+    
     // Update all theme toggle icons
     document.querySelectorAll(".theme-toggle img").forEach((img) => {
       img.src =
         theme === "dark"
-          ? "/assets/files/svg/Sun.svg"
-          : "/assets/files/svg/Moon.svg";
+          ? `${basePath}assets/files/svg/Sun.svg`
+          : `${basePath}assets/files/svg/Moon.svg`;
     });
   };
 
